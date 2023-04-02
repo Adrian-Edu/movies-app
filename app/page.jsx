94b15dc4e2cb9ad2ac5f0d346f652e4f6f85ba19
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-04-02 13:02:38 +0300

    modificari

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-lg py-2 m-4">Home page</h1>
+      <h1 className="text-lg py-2 m-4">Home pag !!!</h1>
     </main>
   );
 }
