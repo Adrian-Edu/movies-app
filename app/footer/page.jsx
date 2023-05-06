4ee8e654cc442ee67379fd083e678efbbf5dcf45
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2023-05-06 21:21:34 +0300

    modificari

--- app/footer/page.jsx
+++ app/footer/page.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className="text-center text-lg  md:text-3xl  bg-black text-yellow-400 py-10 relative bottom-[-50px] ">
+    <section className="text-center text-lg  md:text-3xl  bg-black text-yellow-400 py-5 relative bottom-[-50px] ">
       <div className="grid leading-10">
         <a className="link link-hover">Home Page</a>
         <a className="link link-hover">About us</a>
@@ -14,5 +14,3 @@ export default function Footer() {
     </section>
   );
 }
-
-//text-2xl ... bg-black text-center custom-footer  text-yellow-400
